0a9c8198c405f2b650461a26ec035324fccc775f
Stephanie Kasaon <stephaniekasaon@gmail.com>
2022-03-04 00:04:11 +0900

    added allowed urls

--- electionproject/settings.py
+++ electionproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'blalkhflahsdflkahe;fiuha;fkasdh;kajf;dkajfg;jk'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'steph-voting-portal.herokuapp.com']
 
 
 # Application definition
